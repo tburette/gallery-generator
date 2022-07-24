a2ffcaf44fcb4731b23895a93c5e669a71c5b0fe
Thomas Burette <burettethomas@gmail.com>
2022-07-24 14:31:03 +0200

    Generate thumbnails +  refactoring

--- gallery-generator.mjs
+++ gallery-generator.mjs
@@ -1,4 +1,6 @@
 import {readdir, mkdir, open} from 'fs/promises';
+import util from 'util';
+const exec = util.promisify((await import('child_process')).exec);
 
 const OUTPUT_DIRECTORY_NAME = "generatedGallery";
 const INDEX_FILE_NAME = "index.html";
@@ -14,27 +16,45 @@ function isVideo(filename) {
     return videoExtensions.has(filename.split('.').at(-1));
 }
 
-async function generateGalleryForDirectory(indexFile, directory) {
-    let dirContent = await readdir(path + '/' + directory);
+async function generateGalleryForDirectory(inputGalleryName, indexFile, outputDirectory) {
+    let dirContent = await readdir(path + '/' + inputGalleryName);
+    // todo : filter out non image/videos (and directories)
     dirContent.sort((a, b)=>a.localeCompare(b, 'en', {numeric: true}));
-    indexFile.write(`${directory} (${dirContent.length}) <br>`);
+    indexFile.write(`${inputGalleryName} (${dirContent.length}) <br>`);
     const PREVIEW_COUNT = 4;
-    let previewFiles;
+    let filesSelectedForPreview;
     if(dirContent.length <= PREVIEW_COUNT) {
-        previewFiles = dirContent;
+        filesSelectedForPreview = dirContent;
     } else {
-        //always take the first
-        previewFiles = dirContent.slice(0, 1);
+        // always take the first
+        filesSelectedForPreview = dirContent.slice(0, 1);
+        // then space out the remaining ones
         for(let i = 0;i<PREVIEW_COUNT-1;i++) {
             // take from the second image in the files to the last
             let index_to_take = 1 + Math.floor(((dirContent.length-1)/PREVIEW_COUNT-1)*i);
-            previewFiles.push(dirContent[index_to_take]);
+            filesSelectedForPreview.push(dirContent[index_to_take]);
         }
     }
 
-    for(var file of previewFiles) {
-        // must pick img for image type
-        indexFile.write(`<img src="${path}/${directory}/${file}" width="80"></img>`)
+    try {
+        await mkdir(outputDirectory + '/' + inputGalleryName);
+    }catch(err) {
+        if(err?.code != 'EEXIST') {
+            throw err;
+        }
+    }
+
+    for(var previewFilename of filesSelectedForPreview) {
+        let fullpreviewFilePath = path + '/' + inputGalleryName + '/' + previewFilename;
+        let fullthumbnailFilePath = outputDirectory + '/' + inputGalleryName + '/' + 'thumb_' + previewFilename + '.jpg';
+        if(isImage(previewFilename)) {
+            await exec(`convert '${fullpreviewFilePath}' -resize 100x100 '${fullthumbnailFilePath}'`);
+        } else if(isVideo(previewFilename)) {
+            await exec(`ffmpeg -nostdin -y -ss 4 -i '${fullpreviewFilePath}' -vf scale=w=120:h=120:force_original_aspect_ratio=decrease -frames:v 1 '${fullthumbnailFilePath}'`);
+        } else {
+            throw new Error(`Asked to create preview for file which is neither an image nor a video: ${previewFilename}`);
+        }
+        indexFile.write(`<img src="${fullthumbnailFilePath}"></img>`)
     }
     indexFile.write('<br><br>');
 }
@@ -66,6 +86,7 @@ try {
 }catch(err) {
     if(err.code != 'EEXIST') {
         console.error(`Couldn't create the outpout directory (${outputDirectory})`);
+        process.exit(1);
     }
 }
 
@@ -80,9 +101,9 @@ try{
 
 indexFile.write(`<h1>${path.split('/').at(-1)} Gallery</h1>\n`);
 
-for(let directory of directories) {
-    if(directory == OUTPUT_DIRECTORY_NAME) continue;
-    await generateGalleryForDirectory(indexFile, directory);
+for(let inputGalleryName of directories) {
+    if(inputGalleryName == OUTPUT_DIRECTORY_NAME) continue;
+    await generateGalleryForDirectory(inputGalleryName, indexFile, outputDirectory);
 }
 
 indexFile.close();
