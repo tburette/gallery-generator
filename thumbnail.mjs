cad67f4b3232f6838ce7f686cb76b016ad13c209
Thomas Burette <burettethomas@gmail.com>
2022-07-25 16:40:38 +0200

    Constants instead of hardcoded thumbnail size

--- thumbnail.mjs
+++ thumbnail.mjs
@@ -7,6 +7,8 @@ export {generateThumbnail}
 
 const THUMBNAIL_FILE_PREFIX = 'thumb_';
 const THUMBNAIL_FILE_POSTFIX = '.jpg';
+const THUMBNAIL_WIDTH = 120;
+const THUMBNAIL_HEIGHT = 120;
 
 async function generateThumbnail(inputFilePath, destinationDirectory) {
     let inputFileName = basename(inputFilePath);
@@ -20,9 +22,9 @@ async function generateThumbnail(inputFilePath, destinationDirectory) {
         THUMBNAIL_FILE_POSTFIX;
     if(isImage(inputFileName)) {
         // [0] to convert only the first frame of gifs
-        await exec(`convert '${inputFilePath}[0]' -resize 100x100 '${thumbnailFilePath}'`);
+        await exec(`convert '${inputFilePath}[0]' -resize ${THUMBNAIL_WIDTH}x${THUMBNAIL_HEIGHT} '${thumbnailFilePath}'`);
     } else if(isVideo(inputFileName)) {
-        await exec(`ffmpeg -nostdin -y -ss 4 -i '${inputFilePath}' -vf scale=w=120:h=120:force_original_aspect_ratio=decrease -frames:v 1 '${thumbnailFilePath}'`);
+        await exec(`ffmpeg -nostdin -y -ss 4 -i '${inputFilePath}' -vf scale=w=${THUMBNAIL_WIDTH}:h=${THUMBNAIL_HEIGHT}:force_original_aspect_ratio=decrease -frames:v 1 '${thumbnailFilePath}'`);
     } else {
         // TODO silently ignore or log instead?
         throw new Error(`Asked to create preview for file which is neither an image nor a video: ${inputFilePath}`);
